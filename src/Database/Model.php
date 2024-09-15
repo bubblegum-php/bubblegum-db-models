49d14cb44df963ddf19bef869777f6200a172c45
Oleksii Fedorenko <lankrf@gmail.com>
2024-09-15 13:58:25 +0300

    Iterator interface implementation

--- src/Database/Model.php
+++ src/Database/Model.php
@@ -6,13 +6,17 @@ use Bubblegum\Database\Condition;
 use Bubblegum\Database\DB;
 use Bubblegum\Database\ConditionsUnion;
 
-class Model
+class Model implements \Iterator
 {
     protected static string $tableName;
 
     protected ConditionsUnion $conditions;
 
     protected \PDOStatement $statement;
+
+    protected array|false $data;
+
+    protected int $currentIndex;
 
     public function __construct()
     {}
@@ -44,5 +48,32 @@ class Model
     {
         $this->get();
         return $this->statement->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function current(): Model
+    {
+        return $this;
+    }
+
+    public function next(): void
+    {
+        $this->currentIndex++;
+        $this->data = $this->statement->fetch(\PDO::FETCH_ASSOC);
+    }
+
+    public function key(): int
+    {
+        return $this->currentIndex;
+    }
+
+    public function valid(): bool
+    {
+        return $this->data !== false;
+    }
+
+    public function rewind(): void
+    {
+        $this->currentIndex = 0;
+        $this->data = $this->statement->fetch(\PDO::FETCH_ASSOC);
     }
 }
